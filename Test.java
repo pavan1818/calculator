a5cc7e0f1784a564c2a1880afe3e15491d42ee31
pavan bhujbal <pavanbhujbal18@gmail.com>
2022-08-19 16:16:24 +0530

    One feature added to test.java

--- Test.java
+++ Test.java
@@ -3,14 +3,9 @@ package session_01;
 public class Test {
 		
 	int data =10 ;		//instance variable
-	static int no = 555;// static variable
+	int no = 555;// static variable
 	
-	public void myMethod()
-	{
-		int no = 1;		//local variable
-	}
-	
-	
-	
+	int sum = data+no;
+	System.out.println("Sum of two number is: "+sum);
 	
 }
